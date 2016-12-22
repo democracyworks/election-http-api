e07bd4b2a99a9c52212dfba06c09a47c04e0db9e
Christopher Shea <cmshea@gmail.com>
2016-12-22 14:52:06 -0500

    Kehaar 0.8.1

--- src/election_http_api/queue.clj
+++ src/election_http_api/queue.clj
@@ -1,56 +1,22 @@
 (ns election-http-api.queue
   (:require [clojure.tools.logging :as log]
             [langohr.core :as rmq]
-            [kehaar.core :as k]
-            [kehaar.wire-up :as wire-up]
+            [kehaar.configured :as kehaar]
             [kehaar.rabbitmq]
-            [election-http-api.channels :as channels]
-            [election-http-api.handlers :as handlers]
             [turbovote.resource-config :refer [config]]))
 
 (defn initialize []
   (let [max-retries 5
         rabbit-config (config [:rabbitmq :connection])
-        connection (kehaar.rabbitmq/connect-with-retries rabbit-config max-retries)]
-    (let [incoming-events []
-          incoming-services [(wire-up/incoming-service
-                              connection
-                              "election-http-api.ok"
-                              (config [:rabbitmq :queues "election-http-api.ok"])
-                              channels/ok-requests
-                              channels/ok-responses)]
-          external-services [(wire-up/external-service
-                              connection
-                              ""
-                              "election-works.election.search"
-                              (config [:rabbitmq :queues "election-works.election.search"])
-                              10000
-                              channels/election-upcoming-search)
-                             (wire-up/external-service
-                              connection
-                              ""
-                              "electorate-works.electorate.search-create"
-                              (config [:rabbitmq :queues
-                                       "electorate-works.electorate.search-create"])
-                              10000
-                              channels/electorate-search-create)]
-          outgoing-events []]
-
-      (wire-up/start-responder! channels/ok-requests
-                                channels/ok-responses
-                                handlers/ok)
-
-      {:connections [connection]
-       :channels (vec (concat
-                       incoming-events
-                       incoming-services
-                       external-services
-                       outgoing-events))})))
+        connection (kehaar.rabbitmq/connect-with-retries rabbit-config max-retries)
+        kehaar-resources (kehaar/init! connection (config [:rabbitmq :kehaar]))]
+    {:connections [connection]
+     :kehaar-resources kehaar-resources}))
 
 (defn close-resources! [resources]
   (doseq [resource resources]
     (when-not (rmq/closed? resource) (rmq/close resource))))
 
-(defn close-all! [{:keys [connections channels]}]
-  (close-resources! channels)
+(defn close-all! [{:keys [connections kehaar-resources]}]
+  (kehaar/shutdown! kehaar-resources)
   (close-resources! connections))
