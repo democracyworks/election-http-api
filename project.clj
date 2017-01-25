79871eb9d417502501fad94981c57653d838565a
Wes Morgan <wesmorgan@icloud.com>
2017-01-25 12:50:22 -0700

    Update deps

--- project.clj
+++ project.clj
@@ -15,7 +15,11 @@
                  ;; kehaar.core/go-handler. (This may not be true anymore in
                  ;; core.async 0.2.x; need to test.)
                  [org.clojure/core.async "0.2.395"]
+                 <<<<<<< HEAD
                  [democracyworks/kehaar "0.9.0"]
+                 =======
+                 [democracyworks/kehaar "0.8.1"]
+                 >>>>>>> Update deps
 
                  [io.pedestal/pedestal.service "0.5.2"]
                  [io.pedestal/pedestal.service-tools "0.5.2"]
